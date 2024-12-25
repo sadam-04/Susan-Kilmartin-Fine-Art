b926bc51fb4fa05d98d3ddf9f789f49c4b1b1783
sadam-04 <asaich3@yahoo.com>
2024-12-25 03:03:54 -0500

    homepage

--- skfa/urls.py
+++ skfa/urls.py
@@ -21,4 +21,5 @@ from skfa import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage, name="home"),
+    path('portfolio', views.portfolio, name="portfolio"),
 ]
--- skfa/views.py
+++ skfa/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 def homepage(request):
     return render(request, "templates/homepage.html")
+
+def portfolio(request):
+    return render(request, "templates/portfolio.html")
