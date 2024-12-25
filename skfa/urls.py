aa36f5279f9bb4211649418fef74771bf21d8637
sadam-04 <asaich3@yahoo.com>
2024-12-24 21:50:06 -0500

    homepage skeleton

--- skfa/urls.py
+++ skfa/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from skfa import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homepage, name="home"),
 ]
